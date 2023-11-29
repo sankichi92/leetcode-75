432b8acb228ac3428b950aa0b547d2f79c18d4c3
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-11-29 21:40:16 +0900

    05: Re-submit

--- 05-reverse-vowels-of-a-string/src/main.rs
+++ 05-reverse-vowels-of-a-string/src/main.rs
@@ -3,14 +3,14 @@ fn main() {}
 pub fn reverse_vowels(s: String) -> String {
     let mut vowels = Vec::new();
     for c in s.chars() {
-        if ['a', 'e', 'i', 'o', 'u'].contains(&c) {
+        if ['a', 'e', 'i', 'o', 'u'].contains(&c.to_ascii_lowercase()) {
             vowels.push(c);
         }
     }
 
     let mut results = String::with_capacity(s.len());
     for c in s.chars() {
-        results.push(if ['a', 'e', 'i', 'o', 'u'].contains(&c) {
+        results.push(if ['a', 'e', 'i', 'o', 'u'].contains(&c.to_ascii_lowercase()) {
             vowels.pop().unwrap()
         } else {
             c
@@ -34,5 +34,11 @@ mod tests {
     fn case2() {
         let result: String = reverse_vowels("leetcode".to_string());
         assert_eq!(result, "leotcede");
+    }
+
+    #[test]
+    fn failed_case1() {
+        let result: String = reverse_vowels("aA".to_string());
+        assert_eq!(result, "Aa");
     }
 }
