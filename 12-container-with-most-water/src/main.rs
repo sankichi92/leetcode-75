fff9dd15515034f6af34c6f346f4ccc5ab62a128
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-02 14:43:33 +0900

    12: Submit

---
+++ 12-container-with-most-water/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {}
+
+pub fn max_area(height: Vec<i32>) -> i32 {
+    let mut max_area = 0;
+
+    for (i, val1) in height.iter().enumerate() {
+        for (j, val2) in height.iter().skip(i + 1).enumerate() {
+            let area = val1.min(val2) * (j as i32 + 1);
+            if area > max_area {
+                max_area = area;
+            }
+        }
+    }
+
+    max_area
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(max_area(vec![1, 1]), 1);
+    }
+}
