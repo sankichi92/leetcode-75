3172d1f6b387ebb643411de6e1341eb104072d82
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-02 14:19:23 +0900

    11: Submit

---
+++ 11-is-subsequence/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {}
+
+pub fn is_subsequence(s: String, t: String) -> bool {
+    let mut s: Vec<_> = s.chars().rev().collect();
+    
+    let mut current;
+    if let Some(first) = s.pop() {
+        current = first
+    } else {
+        return true;
+    };
+
+    for char in t.chars() {
+        if char == current {
+            if let Some(next) = s.pop() {
+                current = next;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert!(is_subsequence("abc".to_string(), "ahbgdc".to_string()));
+    }
+
+    #[test]
+    fn case2() {
+        assert!(!is_subsequence("axc".to_string(), "ahbgdc".to_string()));
+    }
+}
