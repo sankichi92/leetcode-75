fcb08349e7d63a13b582783d506ea3c465b39110
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-03 22:41:59 +0900

    14: Submit

---
+++ 14-maximum-average-subarray-i/src/lib.rs
@@ -0,0 +1,27 @@
+pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+    let max = nums.windows(k as usize).fold(0, |max, nums| {
+        let sum = nums.iter().sum();
+        if sum > max {
+            sum
+        } else {
+            max
+        }
+    });
+
+    max as f64 / k as f64
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(find_max_average(vec![1, 12, -5, -6, 50, 3], 4), 12.75);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(find_max_average(vec![5], 1), 5.0);
+    }
+}
