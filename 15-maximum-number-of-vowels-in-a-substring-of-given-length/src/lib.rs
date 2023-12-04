8c52f7d95eaeecba4f1d264f6147484de2c14f1b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-04 21:39:37 +0900

    15: Submit

--- 15-maximum-number-of-vowels-in-a-substring-of-given-length/src/lib.rs
+++ 15-maximum-number-of-vowels-in-a-substring-of-given-length/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn max_vowels(s: String, k: i32) -> i32 {
+    let vowel_letters = ['a', 'e', 'i', 'o', 'u'];
+
+    let mut max: i32 = 0;
+
+    for chars in s.chars().collect::<Vec<_>>().windows(k as usize) {
+        let count = chars.iter().filter(|c| vowel_letters.contains(c)).count() as i32;
+        if count > max {
+            max = count;
+            if max == k {
+                break;
+            }
+        }
+    }
+
+    max
 }
 
 #[cfg(test)]
@@ -7,8 +21,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(max_vowels("abciiidef".to_string(), 3), 3);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(max_vowels("aeiou".to_string(), 2), 2);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(max_vowels("leetcode".to_string(), 3), 2);
     }
 }
