1ca602ac8ddb12c926bb8d06628cc8b8f3ce8f31
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-05 21:47:23 +0900

    17: Submit

--- 17-longest-subarray-of-1s-after-deleting-one-element/src/lib.rs
+++ 17-longest-subarray-of-1s-after-deleting-one-element/src/lib.rs
@@ -1,5 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::VecDeque;
+
+pub fn longest_subarray(nums: Vec<i32>) -> i32 {
+    let first_zero_index = match nums.iter().enumerate().find(|(_, &num)| num == 0) {
+        Some((i, _)) => i,
+        None => return nums.len() as i32 - 1,
+    };
+    let mut subarray: VecDeque<_> = nums.iter().take(first_zero_index + 1).collect();
+    let mut longest = first_zero_index + 1;
+
+    for num in nums.iter().skip(first_zero_index + 1) {
+        if *num == 0 {
+            while let Some(1) = subarray.pop_front() {}
+        }
+
+        subarray.push_back(num);
+        longest = longest.max(subarray.len());
+    }
+
+    (longest as i32) - 1
 }
 
 #[cfg(test)]
@@ -7,8 +25,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(longest_subarray(vec![1, 1, 0, 1]), 3);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(longest_subarray(vec![0, 1, 1, 1, 0, 1, 1, 0, 1]), 5);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(longest_subarray(vec![1, 1, 1]), 2);
     }
 }
