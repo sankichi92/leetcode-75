cfc29c1ffde0ed0dc2bf3712ceb26a63cd7f3fd5
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-06 21:36:44 +0900

    19: Submit

--- 19-find-pivot-index/src/lib.rs
+++ 19-find-pivot-index/src/lib.rs
@@ -1,5 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn pivot_index(nums: Vec<i32>) -> i32 {
+    let mut left_sum = 0;
+    let mut right_sum = nums.iter().sum();
+
+    for (i, num) in nums.iter().enumerate() {
+        right_sum -= num;
+
+        if left_sum == right_sum {
+            return i as i32;
+        }
+
+        left_sum += num;
+    }
+
+    -1
 }
 
 #[cfg(test)]
@@ -7,8 +20,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(pivot_index(vec![1, 7, 3, 6, 5, 6]), 3);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(pivot_index(vec![1, 2, 3]), -1);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(pivot_index(vec![2, 1, -1]), 0);
     }
 }
