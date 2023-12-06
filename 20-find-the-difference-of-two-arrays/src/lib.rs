b74fd091adf2a83e5151678ebc7a4550b3992976
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-06 22:04:02 +0900

    20: Submit

--- 20-find-the-difference-of-two-arrays/src/lib.rs
+++ 20-find-the-difference-of-two-arrays/src/lib.rs
@@ -1,5 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashSet;
+
+pub fn find_difference(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<Vec<i32>> {
+    let nums1: HashSet<i32> = HashSet::from_iter(nums1);
+    let nums2: HashSet<i32> = HashSet::from_iter(nums2);
+
+    vec![
+        nums1.difference(&nums2).copied().collect(),
+        nums2.difference(&nums1).copied().collect(),
+    ]
 }
 
 #[cfg(test)]
@@ -7,8 +15,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        let mut results = find_difference(vec![1, 2, 3], vec![2, 4, 6]);
+        results[0].sort();
+        results[1].sort();
+        assert_eq!(results[0], vec![1, 3]);
+        assert_eq!(results[1], vec![4, 6]);
+    }
+
+    #[test]
+    fn case2() {
+        let mut results = find_difference(vec![1, 2, 3, 3], vec![1, 1, 2, 2]);
+        results[0].sort();
+        results[1].sort();
+        assert_eq!(results[0], vec![3]);
+        assert_eq!(results[1], vec![]);
     }
 }
