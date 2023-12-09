0e6408ab96668eaddc30e7c2300d9efdf711a619
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-10 00:45:38 +0900

    28: Re-submit

--- 28-dota2-senate/src/lib.rs
+++ 28-dota2-senate/src/lib.rs
@@ -1,28 +1,63 @@
+use std::collections::VecDeque;
+
 pub fn predict_party_victory(senate: String) -> String {
-    let (mut r, mut d) = senate.chars().fold((0, 0), |(mut r, mut d), senator| {
-        match senator {
-            'R' => r += 1,
-            'D' => d += 1,
-            _ => (),
-        }
-        (r, d)
-    });
+    let mut senate: VecDeque<_> = senate.chars().collect();
 
-    for senator in senate.chars() {
-        match senator {
-            'R' => d -= 1,
-            'D' => r -= 1,
-            _ => (),
+    loop {
+        let mut r = 0;
+        let mut d = 0;
+
+        let current_senate = senate.clone();
+        senate.clear();
+
+        for senator in current_senate {
+            match senator {
+                'R' => {
+                    if d > 0 {
+                        d -= 1;
+                    } else {
+                        r += 1;
+                        senate.push_back(senator);
+                    }
+                }
+                'D' => {
+                    if r > 0 {
+                        r -= 1;
+                    } else {
+                        d += 1;
+                        senate.push_back(senator);
+                    }
+                }
+                _ => panic!(),
+            }
         }
 
-        match r {
-            r if r > d => return String::from("Radiant"),
-            r if r < d => return String::from("Dire"),
-            _ => ()
+        while r > 0 || d > 0 {
+            match senate.pop_front() {
+                Some('R') => {
+                    if d > 0 {
+                        d -= 1;
+                    } else {
+                        senate.push_front('R');
+                    }
+                }
+                Some('D') => {
+                    if r > 0 {
+                        r -= 1;
+                    } else {
+                        senate.push_front('R');
+                    }
+                }
+                _ => break,
+            }
+        }
+
+        match senate.iter().filter(|&&senator| senator == 'R').count() {
+            r if r == senate.len() => return String::from("Radiant"),
+            0 => return String::from("Dire"),
+            _ => (),
         }
     }
-
-    panic!();
 }
 
 #[cfg(test)]
@@ -40,5 +75,13 @@ mod tests {
     #[test]
     fn case2() {
         assert_eq!(predict_party_victory("RDD".to_string()), "Dire".to_string())
+    }
+
+    #[test]
+    fn failed_case1() {
+        assert_eq!(
+            predict_party_victory("DRDRR".to_string()),
+            "Dire".to_string()
+        )
     }
 }
