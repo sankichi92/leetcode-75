2d2b6bc2cf9f2d7ab23d05eb3dd3fd5785ccf69a
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-09 23:36:35 +0900

    28: Submit

--- 28-dota2-senate/src/lib.rs
+++ 28-dota2-senate/src/lib.rs
@@ -1,5 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn predict_party_victory(senate: String) -> String {
+    let (mut r, mut d) = senate.chars().fold((0, 0), |(mut r, mut d), senator| {
+        match senator {
+            'R' => r += 1,
+            'D' => d += 1,
+            _ => (),
+        }
+        (r, d)
+    });
+
+    for senator in senate.chars() {
+        match senator {
+            'R' => d -= 1,
+            'D' => r -= 1,
+            _ => (),
+        }
+
+        match r {
+            r if r > d => return String::from("Radiant"),
+            r if r < d => return String::from("Dire"),
+            _ => ()
+        }
+    }
+
+    panic!();
 }
 
 #[cfg(test)]
@@ -7,8 +30,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(
+            predict_party_victory("RD".to_string()),
+            "Radiant".to_string()
+        )
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(predict_party_victory("RDD".to_string()), "Dire".to_string())
     }
 }
