d7705a34a19bba6f05a321e5fdf5f1e231e05f30
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-09 12:11:14 +0900

    22: Submit

--- 22-determine-if-two-strings-are-close/src/lib.rs
+++ 22-determine-if-two-strings-are-close/src/lib.rs
@@ -1,5 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashSet;
+
+pub fn close_strings(word1: String, word2: String) -> bool {
+    if word1.len() != word2.len() {
+        return false;
+    }
+
+    let word1: HashSet<_> = word1.chars().collect();
+    let word2: HashSet<_> = word2.chars().collect();
+
+    if word1 != word2 {
+        return false;
+    }
+
+    true
 }
 
 #[cfg(test)]
@@ -7,8 +20,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert!(close_strings("abc".to_string(), "bca".to_string()));
+    }
+
+    #[test]
+    fn case2() {
+        assert!(!close_strings("a".to_string(), "aa".to_string()));
+    }
+
+    #[test]
+    fn case3() {
+        assert!(close_strings("cabbba".to_string(), "abbccc".to_string()));
     }
 }
