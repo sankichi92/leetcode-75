dbff2cebe278d16167088b6bd282d0bf75629dfc
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-09 15:09:11 +0900

    25: Re-submit

--- 25-asteroid-collision/src/lib.rs
+++ 25-asteroid-collision/src/lib.rs
@@ -1,22 +1,28 @@
 pub fn asteroid_collision(asteroids: Vec<i32>) -> Vec<i32> {
-    asteroids.into_iter().fold(Vec::new(), |mut results, asteroid| {
-        if asteroid < 0 {
-            while let Some(previous) = results.pop() {
-                match previous {
-                    size if size > 0 && size > asteroid.abs() => {
-                        results.push(previous);
-                        break
+    asteroids
+        .iter()
+        .skip(1)
+        .fold(Vec::from([asteroids[0]]), |mut results, asteroid| {
+            if *asteroid < 0 {
+                while let Some(previous) = results.pop() {
+                    match previous {
+                        size if size > 0 && size > asteroid.abs() => {
+                            results.push(previous);
+                            break;
+                        }
+                        size if size > 0 && size == asteroid.abs() => break,
+                        size if size < 0 => {
+                            results.extend([previous, *asteroid]);
+                            break;
+                        }
+                        _ => (),
                     }
-                    size if size > 0 && size == asteroid.abs() => break,
-                    size if size < 0 => results.push(asteroid),
-                    _ => ()
                 }
+            } else {
+                results.push(*asteroid);
             }
-        } else {
-            results.push(asteroid);
-        }
-        results
-    })
+            results
+        })
 }
 
 #[cfg(test)]
@@ -36,5 +42,10 @@ mod tests {
     #[test]
     fn case3() {
         assert_eq!(asteroid_collision(vec![10, 2, -5]), vec![10])
+    }
+
+    #[test]
+    fn failed_case1() {
+        assert_eq!(asteroid_collision(vec![-2, -1, 1, 2]), vec![-2, -1, 1, 2])
     }
 }
