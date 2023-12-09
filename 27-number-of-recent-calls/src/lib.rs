5d184657719a984102634cbc0ec7d34841e53f8c
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-09 22:55:31 +0900

    27: Submit

--- 27-number-of-recent-calls/src/lib.rs
+++ 27-number-of-recent-calls/src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct RecentCounter {
+    requests: Vec<i32>
+}
+
+impl RecentCounter {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        RecentCounter { requests: Vec::new() }
+    }
+
+    pub fn ping(&mut self, t: i32) -> i32 {
+        let mut count = 1;
+        for &request_t in self.requests.iter().rev() {
+            if request_t >= t - 3000 {
+                count += 1;
+            } else {
+                break;
+            }
+        }
+
+        self.requests.push(t);
+        
+        count
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +29,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        let mut recent_counter = RecentCounter::new();
+        assert_eq!(recent_counter.ping(1), 1);
+        assert_eq!(recent_counter.ping(100), 2);
+        assert_eq!(recent_counter.ping(3001), 3);
+        assert_eq!(recent_counter.ping(3002), 3);
     }
 }
