a8cfebdd9b643c71e6043c9d8928906758e1ac0f
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-12 23:02:07 +0900

    29: Submit

--- 29-delete-the-middle-node-of-a-linked-list/src/lib.rs
+++ 29-delete-the-middle-node-of-a-linked-list/src/lib.rs
@@ -1,5 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+pub fn delete_middle(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut size = 0;
+    let mut current = head.as_ref();
+
+    while let Some(boxed_node) = current {
+        size += 1;
+        current = boxed_node.next.as_ref();
+    }
+
+    let middle = size / 2;
+    let mut current = head.as_mut().unwrap();
+
+    for _ in 0..middle - 1 {
+        current = current.next.as_mut().unwrap();
+    }
+    current.next = current.next.as_mut().unwrap().next.take();
+
+    head
 }
 
 #[cfg(test)]
@@ -7,8 +37,77 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(
+            delete_middle(Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode {
+                            val: 7,
+                            next: Some(Box::new(ListNode {
+                                val: 1,
+                                next: Some(Box::new(ListNode {
+                                    val: 2,
+                                    next: Some(Box::new(ListNode::new(6)))
+                                }))
+                            }))
+                        }))
+                    }))
+                }))
+            }))),
+            Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode {
+                            val: 1,
+                            next: Some(Box::new(ListNode {
+                                val: 2,
+                                next: Some(Box::new(ListNode::new(6)))
+                            }))
+                        }))
+                    }))
+                }))
+            }))
+        )
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(
+            delete_middle(Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode {
+                        val: 3,
+                        next: Some(Box::new(ListNode::new(4)))
+                    }))
+                }))
+            }))),
+            Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode::new(4)))
+                }))
+            }))
+        )
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(
+            delete_middle(Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode::new(1)))
+            }))),
+            Some(Box::new(ListNode::new(2)))
+        )
     }
 }
