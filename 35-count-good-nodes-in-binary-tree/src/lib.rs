09df9e6df85662e0dac96efc80e0ac9201b306d1
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-15 12:57:19 +0900

    35: Submit

--- 35-count-good-nodes-in-binary-tree/src/lib.rs
+++ 35-count-good-nodes-in-binary-tree/src/lib.rs
@@ -1,5 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    let root = root.unwrap();
+    let val = root.borrow().val;
+    inner_good_nodes(&root, val)
+}
+
+fn inner_good_nodes(node: &Rc<RefCell<TreeNode>>, max: i32) -> i32 {
+    let node = node.borrow();
+    let max = max.max(node.val);
+    let good = if node.val >= max { 1 } else { 0 };
+
+    match (&node.left, &node.right) {
+        (None, None) => good,
+        (Some(left), None) => good + inner_good_nodes(left, max),
+        (None, Some(right)) => good + inner_good_nodes(right, max),
+        (Some(left), Some(right)) => {
+            good + inner_good_nodes(left, max) + inner_good_nodes(right, max)
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +45,43 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(
+            good_nodes(Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: Some(Rc::new(RefCell::new(TreeNode::new(1)))),
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(5))))
+                }))),
+            })))),
+            4
+        )
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(
+            good_nodes(Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(2)))),
+                }))),
+                right: None,
+            })))),
+            3
+        )
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(good_nodes(Some(Rc::new(RefCell::new(TreeNode::new(1))))), 1)
     }
 }
