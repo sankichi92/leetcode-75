0d640cb066047670efaa90cce95bf6871b3390e7
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-16 11:47:35 +0900

    39: Submit

--- 39-binary-tree-right-side-view/src/lib.rs
+++ 39-binary-tree-right-side-view/src/lib.rs
@@ -1,5 +1,48 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut results = Vec::new();
+    let mut stack = if let Some(root) = root {
+        vec![(Rc::clone(&root), 0)]
+    } else {
+        return results;
+    };
+
+    while let Some((node, depth)) = stack.pop() {
+        let node = node.borrow();
+
+        if depth >= results.len() {
+            results.push(node.val)
+        }
+
+        if let Some(left) = &node.left {
+            stack.push((Rc::clone(left), depth + 1))
+        }
+        if let Some(right) = &node.right {
+            stack.push((Rc::clone(right), depth + 1))
+        }
+    }
+
+    results
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +50,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(
+            right_side_view(Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(5)))),
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+                }))),
+            })))),
+            vec![1, 3, 4]
+        );
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(
+            right_side_view(Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+            })))),
+            vec![1, 3]
+        );
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(right_side_view(None), vec![]);
     }
 }
