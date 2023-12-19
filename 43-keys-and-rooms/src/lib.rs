47bb275ae14fef1be01180d6ea5cb98339778cd4
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-19 23:07:53 +0900

    43: Submit

--- 43-keys-and-rooms/src/lib.rs
+++ 43-keys-and-rooms/src/lib.rs
@@ -1,5 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashSet;
+
+pub fn can_visit_all_rooms(rooms: Vec<Vec<i32>>) -> bool {
+    let mut unlocked_rooms = HashSet::from([0]);
+    let mut unvisited_rooms = vec![0];
+
+    while let Some(room) = unvisited_rooms.pop() {
+        let keys: HashSet<_> = rooms[room as usize].iter().cloned().collect();
+
+        unvisited_rooms.extend(keys.difference(&unlocked_rooms));
+
+        unlocked_rooms.extend(keys);
+        if unlocked_rooms.len() == rooms.len() {
+            return true;
+        }
+    }
+
+    false
 }
 
 #[cfg(test)]
@@ -7,8 +23,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert!(can_visit_all_rooms(vec![vec![1], vec![2], vec![3], vec![]]));
+    }
+
+    #[test]
+    fn case2() {
+        assert!(!can_visit_all_rooms(vec![
+            vec![1, 3],
+            vec![3, 0, 1],
+            vec![2],
+            vec![0]
+        ]));
     }
 }
