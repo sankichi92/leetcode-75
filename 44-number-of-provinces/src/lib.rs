7e47073b3ff8bb05b9ac01868415a5932a9dbf0b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-19 23:32:59 +0900

    44: Submit

--- 44-number-of-provinces/src/lib.rs
+++ 44-number-of-provinces/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::{HashMap, HashSet};
+
+pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+    let mut city_to_province = HashMap::with_capacity(is_connected.len());
+    
+    for (i, row) in is_connected.iter().enumerate() {
+        let province = *city_to_province.entry(i).or_insert(i);
+        for (j, connected) in row.iter().enumerate().skip(i + 1) {
+            if *connected == 1 {
+                city_to_province.entry(j).or_insert(province);
+            }
+        }
+    }
+
+    let provinces: HashSet<_> = city_to_province.values().collect();
+    provinces.len() as i32
 }
 
 #[cfg(test)]
@@ -7,8 +21,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(find_circle_num(vec![vec![1,1,0],vec![1,1,0],vec![0,0,1]]), 2);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(find_circle_num(vec![vec![1,0,0],vec![0,1,0],vec![0,0,1]]), 3);
     }
 }
