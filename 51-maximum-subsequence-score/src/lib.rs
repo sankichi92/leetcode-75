e11eabe1960e735d81363a625ee73f6e556fe96b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-28 21:21:36 +0900

    51: Re-submit

--- 51-maximum-subsequence-score/src/lib.rs
+++ 51-maximum-subsequence-score/src/lib.rs
@@ -5,8 +5,6 @@ use std::{
 
 pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
     let k = k as usize;
-    let mut max_score_indices: HashSet<_> = (0..k).collect();
-    let mut nums1_sum: i32 = nums1.iter().take(k).sum();
     let mut heap1: BinaryHeap<_> = nums1
         .iter()
         .enumerate()
@@ -19,7 +17,9 @@ pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
         .take(k)
         .map(|(i, &val)| ValAndIdx(val, i))
         .collect();
+    let mut nums1_sum: i32 = nums1.iter().take(k).sum();
     let mut max_score = nums1_sum as i64 * heap2.peek().unwrap().0 as i64;
+    let mut max_score_indices: HashSet<_> = (0..k).collect();
 
     for i in k..nums1.len() {
         let (num1, num2) = (nums1[i], nums2[i]);
@@ -31,7 +31,7 @@ pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
         let score2 = (nums1_sum - nums1[val_and_idx2.1] + num1) as i64
             * num2.min(heap2.peek().unwrap_or(&ValAndIdx(num2, i)).0) as i64;
 
-        if score1 > max_score && score1 > score2 {
+        if score1 > max_score && score1 >= score2 {
             max_score = score1;
             nums1_sum = nums1_sum - val_and_idx1.0 + num1;
             max_score_indices.remove(&val_and_idx1.1);
@@ -42,14 +42,14 @@ pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
                 .collect();
             heap1.pop();
             heap1.push(ValAndIdx(num1, i));
-        } else if score2 > max_score && score2 > score1 {
+        } else if score2 > max_score && score2 >= score1 {
             max_score = score2;
             nums1_sum = nums1_sum - nums1[val_and_idx2.1] + num1;
             max_score_indices.remove(&val_and_idx2.1);
             max_score_indices.insert(i);
             heap1 = max_score_indices
                 .iter()
-                .map(|&i| ValAndIdx(nums2[i], i))
+                .map(|&i| ValAndIdx(nums1[i], i))
                 .collect();
             heap2.push(ValAndIdx(num2, i));
         } else {
@@ -106,23 +106,25 @@ mod tests {
 
     #[test]
     fn failed_case2() {
-        assert_eq!(
-            max_score(
-                vec![
-                    93, 463, 179, 2488, 619, 2006, 1561, 137, 53, 1765, 2304, 1459, 1768, 450,
-                    1938, 2054, 466, 331, 670, 1830, 1550, 1534, 2164, 1280, 2277, 2312, 1509, 867,
-                    2223, 1482, 2379, 1032, 359, 1746, 966, 232, 67, 1203, 2474, 944, 1740, 1775,
-                    1799, 1156, 1982, 1416, 511, 1167, 1334, 2344
-                ],
-                vec![
-                    345, 229, 976, 2086, 567, 726, 1640, 2451, 1829, 77, 1631, 306, 2032, 2497,
-                    551, 2005, 2009, 1855, 1685, 729, 2498, 2204, 588, 474, 693, 30, 2051, 1126,
-                    1293, 1378, 1693, 1995, 2188, 1284, 1414, 1618, 2005, 1005, 1890, 30, 895, 155,
-                    526, 682, 2454, 278, 999, 1417, 1682, 995
-                ],
-                42
-            ),
-            1950210
-        )
+        max_score(
+            vec![
+                93, 463, 179, 2488, 619, 2006, 1561, 137, 53, 1765, 2304, 1459, 1768, 450, 1938,
+                2054, 466, 331, 670, 1830, 1550, 1534, 2164, 1280, 2277, 2312, 1509, 867, 2223,
+                1482, 2379, 1032, 359, 1746, 966, 232, 67, 1203, 2474, 944, 1740, 1775, 1799, 1156,
+                1982, 1416, 511, 1167, 1334, 2344,
+            ],
+            vec![
+                345, 229, 976, 2086, 567, 726, 1640, 2451, 1829, 77, 1631, 306, 2032, 2497, 551,
+                2005, 2009, 1855, 1685, 729, 2498, 2204, 588, 474, 693, 30, 2051, 1126, 1293, 1378,
+                1693, 1995, 2188, 1284, 1414, 1618, 2005, 1005, 1890, 30, 895, 155, 526, 682, 2454,
+                278, 999, 1417, 1682, 995,
+            ],
+            42,
+        );
+    }
+
+    #[test]
+    fn failed_case3() {
+        assert_eq!(max_score(vec![2, 1, 14, 12], vec![11, 7, 13, 6], 3), 168)
     }
 }
