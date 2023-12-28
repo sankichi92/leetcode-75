4a55ee2463559da417744177a60639eee6d60988
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-28 17:51:00 +0900

    51: Re-submit

--- 51-maximum-subsequence-score/src/lib.rs
+++ 51-maximum-subsequence-score/src/lib.rs
@@ -5,6 +5,7 @@ pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
     let mut max_score = 0;
 
     'main: loop {
+        println!("{:?}", current_combination);
         let num1: i64 = current_combination.iter().map(|&i| nums1[i] as i64).sum();
         let num2 = current_combination.iter().map(|&i| nums2[i]).min().unwrap() as i64;
         max_score = max_score.max(num1 * num2);
@@ -12,6 +13,9 @@ pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
         for i in (0..k).rev() {
             if current_combination[i] < length - k + i {
                 current_combination[i] += 1;
+                for j in (i + 1)..k {
+                    current_combination[j] = current_combination[i] + j - i
+                }
                 break;
             }
 
@@ -36,5 +40,13 @@ mod tests {
     #[test]
     fn case2() {
         assert_eq!(max_score(vec![4, 2, 3, 1, 1], vec![7, 5, 10, 9, 6], 1), 30)
+    }
+
+    #[test]
+    fn failed_case1() {
+        assert_eq!(
+            max_score(vec![22, 5, 25, 15, 28, 1], vec![22, 30, 25, 25, 9, 18], 3),
+            1364
+        )
     }
 }
