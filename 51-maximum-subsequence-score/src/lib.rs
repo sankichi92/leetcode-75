c83318c4f3615114a268e0e9264e61d3e6e6f2f4
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-28 21:20:03 +0900

    51: Re-Submit

--- 51-maximum-subsequence-score/src/lib.rs
+++ 51-maximum-subsequence-score/src/lib.rs
@@ -1,31 +1,86 @@
+use std::{
+    cmp::{Ord, Ordering},
+    collections::{BinaryHeap, HashSet},
+};
+
 pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
     let k = k as usize;
-    let length = nums1.len();
-    let mut current_combination: Vec<usize> = (0..k).collect();
-    let mut max_score = 0;
+    let mut max_score_indices: HashSet<_> = (0..k).collect();
+    let mut nums1_sum: i32 = nums1.iter().take(k).sum();
+    let mut heap1: BinaryHeap<_> = nums1
+        .iter()
+        .enumerate()
+        .take(k)
+        .map(|(i, &val)| ValAndIdx(val, i))
+        .collect();
+    let mut heap2: BinaryHeap<_> = nums2
+        .iter()
+        .enumerate()
+        .take(k)
+        .map(|(i, &val)| ValAndIdx(val, i))
+        .collect();
+    let mut max_score = nums1_sum as i64 * heap2.peek().unwrap().0 as i64;
 
-    'main: loop {
-        let num1: i64 = current_combination.iter().map(|&i| nums1[i] as i64).sum();
-        let num2 = current_combination.iter().map(|&i| nums2[i]).min().unwrap() as i64;
-        max_score = max_score.max(num1 * num2);
+    for i in k..nums1.len() {
+        let (num1, num2) = (nums1[i], nums2[i]);
 
-        for i in (0..k).rev() {
-            if current_combination[i] < length - k + i {
-                current_combination[i] += 1;
-                for j in (i + 1)..k {
-                    current_combination[j] = current_combination[i] + j - i
-                }
-                break;
-            }
+        let val_and_idx1 = heap1.peek().unwrap();
+        let val_and_idx2 = heap2.pop().unwrap();
 
-            if i == 0 {
-                break 'main;
-            }
+        let score1 = (nums1_sum - val_and_idx1.0 + num1) as i64 * num2.min(val_and_idx2.0) as i64;
+        let score2 = (nums1_sum - nums1[val_and_idx2.1] + num1) as i64
+            * num2.min(heap2.peek().unwrap_or(&ValAndIdx(num2, i)).0) as i64;
+
+        if score1 > max_score && score1 > score2 {
+            max_score = score1;
+            nums1_sum = nums1_sum - val_and_idx1.0 + num1;
+            max_score_indices.remove(&val_and_idx1.1);
+            max_score_indices.insert(i);
+            heap2 = max_score_indices
+                .iter()
+                .map(|&i| ValAndIdx(nums2[i], i))
+                .collect();
+            heap1.pop();
+            heap1.push(ValAndIdx(num1, i));
+        } else if score2 > max_score && score2 > score1 {
+            max_score = score2;
+            nums1_sum = nums1_sum - nums1[val_and_idx2.1] + num1;
+            max_score_indices.remove(&val_and_idx2.1);
+            max_score_indices.insert(i);
+            heap1 = max_score_indices
+                .iter()
+                .map(|&i| ValAndIdx(nums2[i], i))
+                .collect();
+            heap2.push(ValAndIdx(num2, i));
+        } else {
+            heap2.push(val_and_idx2);
         }
     }
 
     max_score
 }
+
+struct ValAndIdx(i32, usize);
+
+impl Ord for ValAndIdx {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+
+impl PartialOrd for ValAndIdx {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for ValAndIdx {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl Eq for ValAndIdx {}
 
 #[cfg(test)]
 mod tests {
@@ -67,7 +122,7 @@ mod tests {
                 ],
                 42
             ),
-            0
+            1950210
         )
     }
 }
