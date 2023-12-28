e2c58a9e0e7f024e9ad9890ac918cf3fb4d14e5a
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-29 00:59:47 +0900

    52: Submit

--- 52-total-cost-to-hire-k-workers/src/lib.rs
+++ 52-total-cost-to-hire-k-workers/src/lib.rs
@@ -1,5 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+pub fn total_cost(mut costs: Vec<i32>, k: i32, candidates: i32) -> i64 {
+    let k = k as usize;
+    let candidates = candidates as usize;
+
+    let mut total_cost: i64 = 0;
+
+    let mut left_candidates: BinaryHeap<_> =
+        costs.iter().take(candidates).map(|c| Reverse(*c)).collect();
+    let mut right_candidates: BinaryHeap<_> = costs
+        .iter()
+        .rev()
+        .take(candidates.min(costs.len() - candidates))
+        .map(|c| Reverse(*c))
+        .collect();
+
+    for _ in 0..k {
+        if left_candidates.peek().unwrap().0
+            <= right_candidates.peek().unwrap_or(&Reverse(i32::MAX)).0
+        {
+            total_cost += left_candidates.pop().unwrap().0 as i64;
+
+            if costs.len() > candidates * 2 {
+                left_candidates.push(Reverse(costs.remove(candidates)));
+            }
+        } else {
+            total_cost += right_candidates.pop().unwrap().0 as i64;
+
+            if costs.len() > candidates * 2 {
+                right_candidates.push(Reverse(costs.remove(costs.len() - candidates - 1)));
+            }
+        }
+    }
+
+    total_cost
 }
 
 #[cfg(test)]
@@ -7,8 +41,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(total_cost(vec![17, 12, 10, 2, 7, 2, 11, 20, 8], 3, 4), 11);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(total_cost(vec![1, 2, 4, 1], 3, 3), 4);
     }
 }
