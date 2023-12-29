9e96fd645912754f8aba961811765f59b36c3803
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-29 12:47:48 +0900

    55: Re-submit

--- 55-find-peak-element/src/lib.rs
+++ 55-find-peak-element/src/lib.rs
@@ -2,12 +2,11 @@ pub fn find_peak_element(mut nums: Vec<i32>) -> i32 {
     let length = nums.len();
     nums.insert(0, i32::MIN);
     nums.push(i32::MIN);
-    nums
-        .iter()
+    nums.iter()
         .skip(1)
         .take(length)
         .enumerate()
-        .find(|(i, num)| nums[*i] < **num && **num > nums[*i + 2])
+        .find(|(i, num)| nums[*i] <= **num && **num >= nums[*i + 2])
         .unwrap()
         .0 as i32
 }
@@ -24,5 +23,10 @@ mod tests {
     #[test]
     fn case2() {
         assert_eq!(find_peak_element(vec![1, 2, 1, 3, 5, 6, 4]), 1)
+    }
+
+    #[test]
+    fn failed_case1() {
+        assert_eq!(find_peak_element(vec![-2147483648]), 0)
     }
 }
