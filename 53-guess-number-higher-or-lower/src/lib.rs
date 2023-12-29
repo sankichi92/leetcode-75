647770a11e9bf7d8c481259eeb3196172bc9734f
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-29 11:08:20 +0900

    53: Submit

--- 53-guess-number-higher-or-lower/src/lib.rs
+++ 53-guess-number-higher-or-lower/src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[allow(non_snake_case,clippy::missing_safety_doc)]
+pub unsafe fn guessNumber(n: i32) -> i32 {
+    bsearch( 1, n)
+}
+
+unsafe fn bsearch(min: i32, max: i32) -> i32 {
+    let current = min + (max - min) / 2;
+    println!("min={}, max={}, current={}", min, max, current);
+    match guess(current) {
+        -1 => bsearch(min, current),
+        0 => current,
+        1 => bsearch( current, max),
+        _ => panic!(),
+    }
+}
+
+static mut PICK: i32 = 0;
+
+unsafe fn guess(num: i32) -> i32 {
+    match num.cmp(&PICK) {
+        std::cmp::Ordering::Greater => -1,
+        std::cmp::Ordering::Equal => 0,
+        std::cmp::Ordering::Less => 1,
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +29,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        unsafe {
+            PICK = 6;
+            assert_eq!(guessNumber(10), 6);
+        }
+    }
+
+    #[test]
+    fn case2() {
+        unsafe {
+            PICK = 1;
+            assert_eq!(guessNumber(1), 1);
+        }
+    }
+
+    #[test]
+    fn case3() {
+        unsafe {
+            PICK = 1;
+            assert_eq!(guessNumber(2), 1);
+        }
     }
 }
