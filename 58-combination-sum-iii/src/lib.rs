c71d143398451493bc1866ccf1d1a1317552752a
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-29 21:36:12 +0900

    58: Submit

--- 58-combination-sum-iii/src/lib.rs
+++ 58-combination-sum-iii/src/lib.rs
@@ -1,5 +1,26 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn combination_sum3(k: i32, n: i32) -> Vec<Vec<i32>> {
+    let mut results = vec![];
+    let mut candidate: Vec<_> = (1..=k).collect();
+    
+    'main: loop {
+        if candidate.iter().sum::<i32>() == n {
+            results.push(candidate.clone());
+        }
+
+        for i in (0..k).rev() {
+            if candidate[i as usize] < 9 - i {
+                candidate[i as usize] += 1;
+                for j in i + 1..k {
+                    candidate[j as usize] = candidate[i as usize] + j - i;
+                }
+                continue 'main;
+            }
+        }
+
+        break;
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -7,8 +28,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        let results = combination_sum3(3, 7);
+        assert!(results.contains(&vec![1, 2, 4]), "{:?}", results);
+    }
+
+    #[test]
+    fn case2() {
+        let results = combination_sum3(3, 9);
+        assert!(results.contains(&vec![1, 2, 6]), "{:?}", results);
+        assert!(results.contains(&vec![1, 3, 5]), "{:?}", results);
+        assert!(results.contains(&vec![2, 3, 4]), "{:?}", results);
+    }
+
+    #[test]
+    fn case3() {
+        let results = combination_sum3(4, 1);
+        assert_eq!(results, Vec::<Vec<i32>>::new());
     }
 }
