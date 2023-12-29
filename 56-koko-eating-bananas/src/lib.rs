189bba84a2af0321911047e8be302d3765d6c7c5
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-29 13:20:37 +0900

    56: Submit

--- 56-koko-eating-bananas/src/lib.rs
+++ 56-koko-eating-bananas/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn min_eating_speed(mut piles: Vec<i32>, h: i32) -> i32 {
+    piles.sort_unstable();
+    let candidates: Vec<_> = (1..*piles.last().unwrap()).collect();
+    candidates.partition_point(|candidate| {
+        let hours: i32 = piles.iter().map(|pile| (*pile as f64 / *candidate as f64).ceil() as i32).sum();
+        hours > h
+    }) as i32 + 1
 }
 
 #[cfg(test)]
@@ -7,8 +12,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(min_eating_speed(vec![3, 6, 7, 11], 8), 4)
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(min_eating_speed(vec![30, 11, 23, 4, 20], 5), 30)
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(min_eating_speed(vec![30, 11, 23, 4, 20], 6), 23)
     }
 }
