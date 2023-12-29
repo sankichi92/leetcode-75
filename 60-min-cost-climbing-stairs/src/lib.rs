37b69527269f5fc6f5af164624f005170b43afed
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-29 23:11:57 +0900

    60: Submit

--- 60-min-cost-climbing-stairs/src/lib.rs
+++ 60-min-cost-climbing-stairs/src/lib.rs
@@ -1,5 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
+    let mut results = 0;
+    let mut current = cost.len();
+
+    loop {
+        if current == 0 || current == 1 {
+            return results;
+        }
+
+        if cost[current - 1] < cost[current - 2] {
+            results += cost[current - 1];
+            current -= 1
+        } else {
+            results += cost[current - 2];
+            current -= 2
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +22,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(min_cost_climbing_stairs(vec![10, 15, 20]), 15);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(
+            min_cost_climbing_stairs(vec![1, 100, 1, 1, 1, 100, 1, 1, 100, 1]),
+            6
+        );
     }
 }
