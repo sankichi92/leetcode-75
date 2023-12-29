73cb6e852f50407026c096b41306b840cf3592d5
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-29 11:12:18 +0900

    53: Re-submit

--- 53-guess-number-higher-or-lower/src/lib.rs
+++ 53-guess-number-higher-or-lower/src/lib.rs
@@ -5,11 +5,10 @@ pub unsafe fn guessNumber(n: i32) -> i32 {
 
 unsafe fn bsearch(min: i32, max: i32) -> i32 {
     let current = min + (max - min) / 2;
-    println!("min={}, max={}, current={}", min, max, current);
     match guess(current) {
-        -1 => bsearch(min, current),
+        -1 => bsearch(min, current - 1),
         0 => current,
-        1 => bsearch( current, max),
+        1 => bsearch( current + 1, max),
         _ => panic!(),
     }
 }
@@ -49,6 +48,14 @@ mod tests {
         unsafe {
             PICK = 1;
             assert_eq!(guessNumber(2), 1);
+        }
+    }
+
+    #[test]
+    fn failed_case1() {
+        unsafe {
+            PICK = 2;
+            assert_eq!(guessNumber(2), 2);
         }
     }
 }
