ed5f462176666ca7e4f9a04503815808612ca0d6
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-30 17:07:53 +0900

    61: Re-submit

--- 61-house-robber/src/lib.rs
+++ 61-house-robber/src/lib.rs
@@ -1,4 +1,12 @@
 pub fn rob(nums: Vec<i32>) -> i32 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+
+    if nums.len() == 2 {
+        return nums[0].max(nums[1]);
+    }
+
     let (mut cand1, mut cand2, mut prev) = (nums[0], nums[1], nums[0] + nums[2]);
 
     for num in nums.iter().skip(3) {
@@ -20,5 +28,10 @@ mod tests {
     #[test]
     fn case2() {
         assert_eq!(rob(vec![2, 7, 9, 3, 1]), 12);
+    }
+
+    #[test]
+    fn failed_case1() {
+        assert_eq!(rob(vec![0]), 0);
     }
 }
