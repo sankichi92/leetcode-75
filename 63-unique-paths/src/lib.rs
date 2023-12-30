d2cb1ee0f3e7ea196e762158bf59ef5074c5ba0d
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-30 21:02:53 +0900

    63: Submit

--- 63-unique-paths/src/lib.rs
+++ 63-unique-paths/src/lib.rs
@@ -1,5 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn unique_paths(m: i32, n: i32) -> i32 {
+    let (m, n) = (m as usize, n as usize);
+    let mut grid = vec![vec![0; n]; m];
+
+    for i in 0..m {
+        for j in 0..n {
+            grid[i][j] = match (i, j) {
+                (0, 0) => 0,
+                (0, _) => 1,
+                (_, 0) => 1,
+                _ => grid[i - 1][j] + grid[i][j - 1]
+            }
+        }
+    }
+
+    grid[m - 1][n - 1]
 }
 
 #[cfg(test)]
@@ -7,8 +21,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(unique_paths(3, 7), 28);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(unique_paths(3, 2), 3);
     }
 }
