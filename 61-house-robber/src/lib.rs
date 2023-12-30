b8c467a1fff790909e740a6d4fa61204b566cd80
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-30 17:05:32 +0900

    61: Submit

--- 61-house-robber/src/lib.rs
+++ 61-house-robber/src/lib.rs
@@ -1,5 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn rob(nums: Vec<i32>) -> i32 {
+    let (mut cand1, mut cand2, mut prev) = (nums[0], nums[1], nums[0] + nums[2]);
+
+    for num in nums.iter().skip(3) {
+        (cand1, cand2, prev) = (cand2, prev, num + cand1.max(cand2));
+    }
+
+    (cand1 + nums.last().unwrap()).max(cand2.max(prev))
 }
 
 #[cfg(test)]
@@ -7,8 +13,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(rob(vec![1, 2, 3, 1]), 4);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(rob(vec![2, 7, 9, 3, 1]), 12);
     }
 }
