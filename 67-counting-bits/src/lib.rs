7bb92045fa26f043dfa9642de3b9eb45da644059
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-31 17:55:02 +0900

    67: Submit

--- 67-counting-bits/src/lib.rs
+++ 67-counting-bits/src/lib.rs
@@ -1,5 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn count_bits(n: i32) -> Vec<i32> {
+    (0..=n)
+        .map(|i| format!("{i:b}").chars().filter(|&c| c == '1').count() as i32)
+        .collect()
 }
 
 #[cfg(test)]
@@ -7,8 +9,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(count_bits(2), vec![0, 1, 1]);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(count_bits(5), vec![0, 1, 1, 2, 1, 2]);
     }
 }
