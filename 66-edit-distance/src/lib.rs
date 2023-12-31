d6fb8ececa4a68b02d03718ccff6bd7ad83f86d4
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-31 13:41:19 +0900

    66: Submit

--- 66-edit-distance/src/lib.rs
+++ 66-edit-distance/src/lib.rs
@@ -1,20 +1,24 @@
-pub fn min_distance(mut word1: String, mut word2: String) -> i32 {
-    if word1.is_empty() && word2.is_empty() {
-        return 0;
+pub fn min_distance(word1: String, word2: String) -> i32 {
+    fn min_distance(mut word1: String, mut word2: String, last_popped: char) -> i32 {
+        if word1.is_empty() && word2.is_empty() {
+            return 0;
+        }
+    
+        let char1 = word1.pop();
+        let char2 = word2.pop();
+        match (char1, char2) {
+            (Some(char1), Some(char2)) => {
+                if char1 == char2 || char1 == last_popped  {
+                    min_distance(word1, word2, char2)
+                } else {
+                    1 + min_distance(word1, word2, char2)
+                }
+            }
+            _ => 1 + min_distance(word1, word2, '0'),
+        }
     }
 
-    let char1 = word1.pop();
-    let char2 = word2.pop();
-    match (char1, char2) {
-        (Some(char1), Some(char2)) => {
-            if char1 == char2 {
-                min_distance(word1, word2)
-            } else {
-                1 + min_distance(word1, word2)
-            }
-        }
-        _ => 1 + min_distance(word1, word2),
-    }
+    min_distance(word1, word2, '0')
 }
 
 #[cfg(test)]
