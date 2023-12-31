6d0e58848c76de19f0d0a6367269b8c1bacab8a3
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-12-31 15:29:00 +0900

    66: Re-submit

--- 66-edit-distance/src/lib.rs
+++ 66-edit-distance/src/lib.rs
@@ -1,24 +1,28 @@
 pub fn min_distance(word1: String, word2: String) -> i32 {
-    fn min_distance(mut word1: String, mut word2: String, last_popped: char) -> i32 {
-        if word1.is_empty() && word2.is_empty() {
-            return 0;
-        }
+    let (longer, shorter) = if word1.len() >= word2.len() {
+        (word1, word2)
+    } else {
+        (word2, word1)
+    };
     
-        let char1 = word1.pop();
-        let char2 = word2.pop();
-        match (char1, char2) {
-            (Some(char1), Some(char2)) => {
-                if char1 == char2 || char1 == last_popped  {
-                    min_distance(word1, word2, char2)
-                } else {
-                    1 + min_distance(word1, word2, char2)
-                }
+    let mut distances: Vec<_> = (0..=shorter.len()).collect();
+    let mut prev_distances = distances.clone();
+
+    for l_char in longer.chars() {
+        for (j, s_char) in shorter.chars().enumerate() {
+            if j == 0 {
+                distances[0] += 1
             }
-            _ => 1 + min_distance(word1, word2, '0'),
+            if l_char == s_char {
+                distances[j + 1] = prev_distances[j];
+            } else {
+                distances[j + 1] = distances[j + 1].min(distances[j]).min(prev_distances[j]) + 1
+            }
         }
+        prev_distances.copy_from_slice(&distances);
     }
 
-    min_distance(word1, word2, '0')
+    *distances.last().unwrap() as i32
 }
 
 #[cfg(test)]
@@ -36,5 +40,10 @@ mod tests {
             min_distance("intention".to_string(), "execution".to_string()),
             5
         );
+    }
+
+    #[test]
+    fn failed_case1() {
+        assert_eq!(min_distance("a".to_string(), "ab".to_string()), 1);
     }
 }
