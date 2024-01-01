38ef9bfd4d5aad59c7862adbd9248ac5f4dc7bf8
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-01-01 21:07:47 +0900

    75: Submit

--- 75-online-stock-span/src/lib.rs
+++ 75-online-stock-span/src/lib.rs
@@ -1,5 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct StockSpanner {
+    prices: Vec<i32>,
+}
+
+impl StockSpanner {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        StockSpanner { prices: vec![] }
+    }
+
+    pub fn next(&mut self, price: i32) -> i32 {
+        let mut span = 1;
+        for prev_price in self.prices.iter().rev() {
+            if price >= *prev_price {
+                span += 1;
+            } else {
+                break;
+            }
+        }
+        self.prices.push(price);
+        span
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +27,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        let mut stock_spanner = StockSpanner::new();
+        assert_eq!(stock_spanner.next(100), 1);
+        assert_eq!(stock_spanner.next(80), 1);
+        assert_eq!(stock_spanner.next(60), 1);
+        assert_eq!(stock_spanner.next(70), 2);
+        assert_eq!(stock_spanner.next(60), 1);
+        assert_eq!(stock_spanner.next(75), 4);
+        assert_eq!(stock_spanner.next(85), 6);
     }
 }
