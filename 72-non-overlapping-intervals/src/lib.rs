d6e718fe9c11e0baa9265598046961d487eaf1aa
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-01-01 16:29:01 +0900

    72: Re-submit

--- 72-non-overlapping-intervals/src/lib.rs
+++ 72-non-overlapping-intervals/src/lib.rs
@@ -1,9 +1,16 @@
-pub fn erase_overlap_intervals(intervals: Vec<Vec<i32>>) -> i32 {
-    let mut overlapped_counts = vec![0; intervals.len()];
-    let mut overlapped_indices = vec![vec![]; intervals.len()];
+use std::collections::HashSet;
 
-    for (i, interval1) in intervals.iter().enumerate() {
-        for (j, interval2) in intervals.iter().enumerate().skip(i + 1) {
+pub fn erase_overlap_intervals(intervals: Vec<Vec<i32>>) -> i32 {
+    let intervals_len = intervals.len();
+    let unique_intervals: HashSet<_> = intervals.into_iter().collect();
+
+    let mut removed = (intervals_len - unique_intervals.len()) as i32;
+
+    let mut overlapped_counts = vec![0; unique_intervals.len()];
+    let mut overlapped_indices = vec![vec![]; unique_intervals.len()];
+
+    for (i, interval1) in unique_intervals.iter().enumerate() {
+        for (j, interval2) in unique_intervals.iter().enumerate().skip(i + 1) {
             if (interval1[0] <= interval2[0] && interval2[0] < interval1[1])
                 || (interval1[0] < interval2[1] && interval2[1] <= interval1[1])
             {
@@ -15,11 +22,8 @@ pub fn erase_overlap_intervals(intervals: Vec<Vec<i32>>) -> i32 {
         }
     }
 
-    // println!("{:?}", overlapped_indices);
-
-    let mut removed = 0;
     loop {
-        // println!("{:?}", overlapped_counts);
+        println!("{:?}", overlapped_counts);
         let (max_idx, count) = overlapped_counts
             .iter_mut()
             .enumerate()
@@ -71,6 +75,22 @@ mod tests {
         assert_eq!(
             erase_overlap_intervals(vec![vec![1, 2], vec![1, 3], vec![1, 4]]),
             2
+        );
+    }
+
+    #[test]
+    fn failed_case2() {
+        assert_eq!(
+            erase_overlap_intervals(vec![
+                vec![0, 2],
+                vec![1, 3],
+                vec![1, 3],
+                vec![2, 4],
+                vec![3, 5],
+                vec![3, 5],
+                vec![4, 6]
+            ]),
+            4
         );
     }
 }
