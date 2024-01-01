7b1b5248c611d82f492220d000a948464b8846e8
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-01-01 16:10:23 +0900

    72: Re-submit

--- 72-non-overlapping-intervals/src/lib.rs
+++ 72-non-overlapping-intervals/src/lib.rs
@@ -3,19 +3,23 @@ pub fn erase_overlap_intervals(intervals: Vec<Vec<i32>>) -> i32 {
     let mut overlapped_indices = vec![vec![]; intervals.len()];
 
     for (i, interval1) in intervals.iter().enumerate() {
-        for (j, interval2) in intervals.iter().enumerate().filter(|(j, _)| *j != i) {
-            if (interval1[0] < interval2[0] && interval2[0] < interval1[1])
-                || (interval1[0] < interval2[1] && interval2[1] < interval1[1])
-                || (interval1[0] == interval2[0] && interval1[1] == interval2[1])
+        for (j, interval2) in intervals.iter().enumerate().skip(i + 1) {
+            if (interval1[0] <= interval2[0] && interval2[0] < interval1[1])
+                || (interval1[0] < interval2[1] && interval2[1] <= interval1[1])
             {
                 overlapped_counts[i] += 1;
+                overlapped_counts[j] += 1;
                 overlapped_indices[i].push(j);
+                overlapped_indices[j].push(i);
             }
         }
     }
 
+    // println!("{:?}", overlapped_indices);
+
     let mut removed = 0;
     loop {
+        // println!("{:?}", overlapped_counts);
         let (max_idx, count) = overlapped_counts
             .iter_mut()
             .enumerate()
@@ -60,5 +64,13 @@ mod tests {
     #[test]
     fn case3() {
         assert_eq!(erase_overlap_intervals(vec![vec![1, 2], vec![2, 3]]), 0);
+    }
+
+    #[test]
+    fn failed_case1() {
+        assert_eq!(
+            erase_overlap_intervals(vec![vec![1, 2], vec![1, 3], vec![1, 4]]),
+            2
+        );
     }
 }
