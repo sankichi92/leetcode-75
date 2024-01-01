bdc2152b8aec6b0c0113328c577b26922e514f5d
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-01-01 20:48:22 +0900

    74: Submit

--- 74-daily-temperatures/src/lib.rs
+++ 74-daily-temperatures/src/lib.rs
@@ -1,5 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+    let mut results = vec![0; temperatures.len()];
+    
+    let mut no_warmer_days = vec![(0, temperatures[0])];
+
+    for (current_day, current_temperature) in temperatures.into_iter().enumerate().skip(1) {
+        while let Some((day, temperature)) = no_warmer_days.pop()  {
+            if current_temperature > temperature {
+                results[day] = (current_day - day) as i32;
+            } else {
+                no_warmer_days.push((day, temperature));
+                break;
+            }
+        }
+
+        no_warmer_days.push((current_day, current_temperature));
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -7,8 +24,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case1() {
+        assert_eq!(
+            daily_temperatures(vec![73, 74, 75, 71, 69, 72, 76, 73]),
+            vec![1, 1, 4, 2, 1, 1, 0, 0]
+        );
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(daily_temperatures(vec![30, 40, 50, 60]), vec![1, 1, 1, 0]);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(daily_temperatures(vec![30, 60, 90]), vec![1, 1, 0]);
     }
 }
